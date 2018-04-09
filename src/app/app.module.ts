6435babe3d1a4f62e70129526a749290b46094ca
Junior Miqueletti <junior.miqueletti@matera.com>
2018-04-09 20:57:36 -0300

    moving navbarComponent to core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { CoreModule } from './core/core.module';
 import { ReleasesModule } from './releases/releases.module';
 import { PeopleModule } from './people/people.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
 
+    CoreModule,
     ReleasesModule,
     PeopleModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
