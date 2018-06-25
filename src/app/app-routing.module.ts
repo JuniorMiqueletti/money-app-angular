750d0b187d4933e15639179ebce072a0c0caa2e8
Junior Miqueletti <juniormiqueletti@gmail.com>
2018-06-25 19:39:43 -0300

    Fix UnauthorizedPageComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
+import { UnauthorizedPageComponent } from './core/unauthorized-page/unauthorized-page.component';
 
 const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: 'page-not-found', component: PageNotFoundComponent },
+    { path: 'unauthorized-page', component: UnauthorizedPageComponent},
     { path: '**', redirectTo: 'page-not-found', pathMatch: 'full' }
   ];
 
